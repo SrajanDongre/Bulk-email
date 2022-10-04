dd7b77aee4eebfbdf338b5cdac23353721af6206
Srajan <srajandongre2905@gmail.com>
2022-10-04 05:01:14 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    secretList: [
+      'SECRET'
+    ],
+    email: {
+      authList: [
+        {
+          host: "smtp.gmail.com",
+          port: 465,
+          user: "abc@gmail.com",
+          pass: "123",
+        },
+      ],
+      toEmail: "123@gmail.com"
+    },
+    port: 3300,
+  }
+  
+  module.exports = config
--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var room= []
-
 /* GET home page. */
-router.get('room', function(req, res, next) {
-  res.send(room);
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
-router.post('/create',(req,res)=>{
-  data={
-    roomID:req.body.roomID,
-    type:req.body.type,
-    amenities:req.body.amenities,
-    price:req.body.price,
-    bookedStatus:"Available",
-    customerName:"",
-    date:"",
-    startTime:"",
-    endTime:""
-  }
-  room.push(data);
-  res.json({
-    message:"Room Available"
-  })
-})
-
-router.post('/newbooking',(req,res)=>{
-  let booked=false;
-  room.map((e)=>{
-    if(e.roomID===req.body.roomID){
-      e.bookedStatus="Occupied";
-      e.customerName=req.body.customerName;
-      e.date=req.body.date;
-      e.startTime=req.body.startTime;
-      e.endTime=req.body.endTime;
-      booked=true;
-    }
-  })
-  if(booked){
-    res.join({
-      message:"Booking Failed",
-    })
-  }
-})
-
-router.get('/booked-room-details',(req,res)=>{
-  let data=[];
-  room.map((e)=>{
-    if(e.bookedStatus=='Occupied'){
-      data.push({
-        roomID:e.roomID,
-        bookedStatus:e.bookedStatus,
-        customerName:e.customerName,
-        date:e.date,
-        startTime:e.startTime,
-        endTime:e.endTime
-      })
-    }
-  })
-  res.send(data);
-})
-
-router.get("/booked-customer-details",(req,res)=>{
-  let data=[];
-  room.map((e)=>{
-    if(e.bookedStatus=="Occupied"){
-      data.push({
-        customerName:e.customerName,
-        roomID:e.roomID,
-        date:e.date,
-        startTime:e.startTime,
-        endTime:e.endTime
-      })
-    }
-  })
-  res.send(data);
-})
-
 
 module.exports = router;
